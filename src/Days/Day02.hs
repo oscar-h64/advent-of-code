4124cc4eb4b1286d04a93e06280327fcdad93cec
Oscar Harris <ozj.harris@gmail.com>
2020-12-02 10:32:01 +0000

    Do Day 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -10,6 +10,8 @@ import qualified Data.Map             as M
 import           Data.Maybe
 import           Data.Set             ( Set )
 import qualified Data.Set             as S
+import           Data.Text            ( Text )
+import qualified Data.Text            as T
 import           Data.Vector          ( Vector )
 import qualified Data.Vector          as V
 import           Data.Void            ( Void )
@@ -18,6 +20,8 @@ import qualified Util.Util            as U
 import           Data.Attoparsec.Text
 
 import qualified Program.RunDay       as R ( runDay )
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import GHC.IO (unsafePerformIO)
 
 --------------------------------------------------------------------------------
 
@@ -29,18 +33,33 @@ runDay = R.runDay inputParser partA partB
 --------------------------------------------------------------------------------
 
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many' $ do
+        min <- decimal
+        string "-"
+        max <- decimal
+        space
+        policyLetter <- letter
+        string ": "
+        pw <- takeTill isEndOfLine
+        skipMany endOfLine
+        pure $ Password min max policyLetter pw
 
 
 --------------------------------------------------------------------------------
 --                                   TYPES                                    --
 --------------------------------------------------------------------------------
+data Password = Password {
+    pwPolicyMin :: Int,
+    pwPolicyMax :: Int,
+    pwPolicyLetter :: Char,
+    pwPassword :: Text
+} deriving Show
 
-type Input = Void
+type Input = [Password]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 
 --------------------------------------------------------------------------------
@@ -48,15 +67,28 @@ type OutputB = Void
 --------------------------------------------------------------------------------
 
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = foldr isValidPassword 0
+    where isValidPassword Password{..} n = 
+            let c = T.count (T.singleton pwPolicyLetter) pwPassword
+            in if c >= pwPolicyMin && c <= pwPolicyMax then n + 1 else n
 
 
 --------------------------------------------------------------------------------
 --                                   PART B                                   --
 --------------------------------------------------------------------------------
 
+xor :: Bool -> Bool -> Bool
+xor True False = True
+xor False True = True
+xor _     _    = False
+
+-- Please god forgive me for the use of index
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = foldr isValidPassword 0
+    where isValidPassword Password{..} n = 
+            if xor (T.index pwPassword (pwPolicyMin - 1) == pwPolicyLetter) 
+                   (T.index pwPassword (pwPolicyMax - 1) == pwPolicyLetter)
+            then n + 1 else n
 
 
 --------------------------------------------------------------------------------
