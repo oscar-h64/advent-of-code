2b7ac4a4631409fb00138c86e191aff025da46a5
Oscar Harris <oscar@oscar-h.com>
2020-12-07 09:46:06 +0000

    Do Day 7 - needs cleaning up badly

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -10,6 +10,8 @@ import qualified Data.Map             as M
 import           Data.Maybe
 import           Data.Set             ( Set )
 import qualified Data.Set             as S
+import           Data.Text            ( Text )
+import qualified Data.Text            as T
 import           Data.Vector          ( Vector )
 import qualified Data.Vector          as V
 import           Data.Void            ( Void )
@@ -17,6 +19,7 @@ import qualified Util.Util            as U
 
 import           Data.Attoparsec.Text
 
+import           Control.Applicative
 import qualified Program.RunDay       as R ( runDay )
 
 --------------------------------------------------------------------------------
@@ -29,34 +32,72 @@ runDay = R.runDay inputParser partA partB
 --------------------------------------------------------------------------------
 
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = parseOne `sepBy` endOfLine
+    where parseOne = do
+            let readColour = fmap T.pack $ do
+                    x <- many1' letter
+                    space
+                    y <- many1' letter
+                    pure $ mconcat [x, " ", y]
+            colour <- readColour
+            space
+            string "bags contain"
+            space
+            let
+            inside <- (string "no other bags." >> pure []) <|> many1' (do
+                number <- decimal
+                space
+                colour' <- readColour
+                space
+                string "bag, " <|> string "bags, "
+                               <|> string "bags."
+                               <|> string "bag."
+                pure (number, colour'))
+            pure $ BagRule colour $ S.fromList inside
 
 
 --------------------------------------------------------------------------------
 --                                   TYPES                                    --
 --------------------------------------------------------------------------------
+data BagRule = BagRule {
+    brColour :: Text,
+    brInside :: Set (Int, Text)
+} deriving (Eq, Show)
 
-type Input = Void
+type Input = [BagRule]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 
 --------------------------------------------------------------------------------
 --                                   PART A                                   --
 --------------------------------------------------------------------------------
 
+canHoldColour :: Text -> BagRule -> Bool
+canHoldColour x = any ((x==) . snd) . S.elems . brInside
+
+recursiveContains :: Text -> [BagRule] -> [BagRule]
+recursiveContains _ []    = []
+recursiveContains x rules = let direct = filter (canHoldColour x) rules
+                                remaining = rules \\ direct
+                            in direct ++ concatMap (flip recursiveContains remaining . brColour) direct
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = S.size . S.fromList . map brColour . recursiveContains "shiny gold"
 
 
 --------------------------------------------------------------------------------
 --                                   PART B                                   --
 --------------------------------------------------------------------------------
 
+recursiveCount :: Text -> [BagRule] -> Int
+recursiveCount x xs = let toCheck = brInside $ head $ filter ((x ==) . brColour) xs
+                      in sum $ map (\(i,y) -> i + i * recursiveCount y xs) $ S.toList toCheck
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = recursiveCount "shiny gold"
 
 
 --------------------------------------------------------------------------------
