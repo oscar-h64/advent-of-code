039479bce441963856faa576ae3d02776a4d6f7c
Oscar Harris <oscar@oscar-h.com>
2020-12-08 10:35:03 +0000

    Do part B

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -50,31 +50,48 @@ type Input = [Instruction]
 
 type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 
 --------------------------------------------------------------------------------
 --                                   PART A                                   --
 --------------------------------------------------------------------------------
 
-recurse :: (Int, Int, Set Int) -> [Instruction] -> Int
-recurse (_,    acc, _   ) []  = acc
-recurse (inst, acc, prev) ins = if inst `S.member` prev then acc else
-    case ins !! inst of
-        NoOp _  -> recurse (inst + 1, acc, inst `S.insert` prev) ins
-        Accum i -> recurse (inst + 1, acc + i, inst `S.insert` prev) ins
-        Jump i  -> recurse (inst + i, acc, inst `S.insert` prev) ins
+recurseA :: (Int, Int, Set Int) -> [Instruction] -> Int
+recurseA (inst, acc, prev) ins
+    | inst > length ins = acc
+    | otherwise         = if inst `S.member` prev then acc else
+        case ins !! inst of
+            NoOp _  -> recurseA (inst + 1, acc, inst `S.insert` prev) ins
+            Accum i -> recurseA (inst + 1, acc + i, inst `S.insert` prev) ins
+            Jump i  -> recurseA (inst + i, acc, inst `S.insert` prev) ins
 
 partA :: Input -> OutputA
-partA = recurse (0, 0, S.empty)
+partA = recurseA (0, 0, S.empty)
 
 
 --------------------------------------------------------------------------------
 --                                   PART B                                   --
 --------------------------------------------------------------------------------
 
+recurseB :: (Int, Int, Set Int) -> [Instruction] -> Maybe Int
+recurseB (inst, acc, prev) ins
+    | inst >= length ins = Just acc
+    | inst < 0          = Nothing
+    | otherwise         = if inst `S.member` prev then Nothing else
+        case ins !! inst of
+            NoOp _  -> recurseB (inst + 1, acc, inst `S.insert` prev) ins
+            Accum i -> recurseB (inst + 1, acc + i, inst `S.insert` prev) ins
+            Jump i  -> recurseB (inst + i, acc, inst `S.insert` prev) ins
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB xs = head $ mapMaybe (recurseB (0,0,S.empty)) allPoss
+    where allPoss = fst $ foldl f ([],[[]]) xs
+          f (accDone, accToDo) = \case
+              NoOp i -> (map (++[NoOp i]) accDone ++ map (++[Jump i]) accToDo, map (++[NoOp i]) accToDo)
+              Jump i -> (map (++[Jump i]) accDone ++ map (++[NoOp i]) accToDo, map (++[Jump i]) accToDo)
+              Accum i -> (map (++[Accum i]) accDone, map (++[Accum i]) accToDo)
+
 
 
 --------------------------------------------------------------------------------
