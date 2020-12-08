c2cf4eeac8c6c20bb5840e0e9ffbec2fff6a034e
Oscar Harris <oscar@oscar-h.com>
2020-12-08 11:14:32 +0000

    Do a lot of neatening

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -4,6 +4,11 @@ module Days.Day08 (runDay) where
 
 --------------------------------------------------------------------------------
 
+import           Control.Applicative  ( Alternative ((<|>)) )
+import           Control.Monad.State
+
+import           Data.Either          ( fromLeft, rights )
+import           Data.Functor         ( ($>) )
 import           Data.List
 import           Data.Map             ( Map )
 import qualified Data.Map             as M
@@ -17,8 +22,6 @@ import qualified Util.Util            as U
 
 import           Data.Attoparsec.Text
 
-import           Control.Applicative  ( Alternative ((<|>)) )
-import           Data.Functor         ( ($>) )
 import qualified Program.RunDay       as R ( runDay )
 
 --------------------------------------------------------------------------------
@@ -31,67 +34,73 @@ runDay = R.runDay inputParser partA partB
 --------------------------------------------------------------------------------
 
 inputParser :: Parser Input
-inputParser = parseInstruction `sepBy` endOfLine
+inputParser = fmap V.fromList $ parseInstruction `sepBy` endOfLine
     where parseInstruction = do
             inst <- string "nop" $> NoOp
                 <|> string "acc" $> Accum
                 <|> string "jmp" $> Jump
             space
-            mult <- char '+' $> 1 <|> char '-' $> -1
-            inst . (* mult) <$> decimal
+            inst <$> signed decimal
 
 
 --------------------------------------------------------------------------------
 --                                   TYPES                                    --
 --------------------------------------------------------------------------------
+
 data Instruction = NoOp Int | Accum Int | Jump Int deriving Show
 
-type Input = [Instruction]
+type Input = Vector Instruction
 
 type OutputA = Int
 
 type OutputB = Int
+
+type ComputerM = State (Int, Int, Set Int, Vector Instruction)
+
+--------------------------------------------------------------------------------
+--                                   SHARED                                   --
+--------------------------------------------------------------------------------
+
+recurse :: ComputerM (Either Int Int)
+recurse = get >>= \(inst, acc, prev, ins) ->
+    if | inst >= V.length ins -> pure $ Right acc
+       | inst < 0           -> pure $ Left acc
+       | otherwise          -> if inst `S.member` prev then pure $ Left acc else
+            case ins V.! inst of
+                NoOp _  -> addLoc >> updateIns 1 >> recurse
+                Accum i -> addLoc >> updateIns 1 >> updateAcc i >> recurse
+                Jump i  -> addLoc >> updateIns i >> recurse
+
+
+updateIns :: Int -> ComputerM ()
+updateIns i = modify (\(w,x,y,z) -> (w+i, x, y, z))
+
+updateAcc :: Int -> ComputerM ()
+updateAcc i = modify (\(w,x,y,z) -> (w, x+i, y, z))
+
+addLoc :: ComputerM ()
+addLoc = modify (\(w,x,y,z) -> (w, x, w `S.insert` y, z))
 
 
 --------------------------------------------------------------------------------
 --                                   PART A                                   --
 --------------------------------------------------------------------------------
 
-recurseA :: (Int, Int, Set Int) -> [Instruction] -> Int
-recurseA (inst, acc, prev) ins
-    | inst > length ins = acc
-    | otherwise         = if inst `S.member` prev then acc else
-        case ins !! inst of
-            NoOp _  -> recurseA (inst + 1, acc, inst `S.insert` prev) ins
-            Accum i -> recurseA (inst + 1, acc + i, inst `S.insert` prev) ins
-            Jump i  -> recurseA (inst + i, acc, inst `S.insert` prev) ins
-
 partA :: Input -> OutputA
-partA = recurseA (0, 0, S.empty)
+partA xs = fromLeft 0 $ evalState recurse (0, 0, S.empty, xs)
 
 
 --------------------------------------------------------------------------------
 --                                   PART B                                   --
 --------------------------------------------------------------------------------
 
-recurseB :: (Int, Int, Set Int) -> [Instruction] -> Maybe Int
-recurseB (inst, acc, prev) ins
-    | inst >= length ins = Just acc
-    | inst < 0          = Nothing
-    | otherwise         = if inst `S.member` prev then Nothing else
-        case ins !! inst of
-            NoOp _  -> recurseB (inst + 1, acc, inst `S.insert` prev) ins
-            Accum i -> recurseB (inst + 1, acc + i, inst `S.insert` prev) ins
-            Jump i  -> recurseB (inst + i, acc, inst `S.insert` prev) ins
-
 partB :: Input -> OutputB
-partB xs = head $ mapMaybe (recurseB (0,0,S.empty)) allPoss
-    where allPoss = fst $ foldl f ([],[[]]) xs
+partB xs = head $ rights $ map (\x -> evalState recurse (0, 0, S.empty, x)) allPoss
+    where allPoss = fst $ foldl f ([],[V.empty]) xs
           f (accDone, accToDo) = \case
-              NoOp i -> (map (++[NoOp i]) accDone ++ map (++[Jump i]) accToDo, map (++[NoOp i]) accToDo)
-              Jump i -> (map (++[Jump i]) accDone ++ map (++[NoOp i]) accToDo, map (++[Jump i]) accToDo)
-              Accum i -> (map (++[Accum i]) accDone, map (++[Accum i]) accToDo)
-
+              NoOp i -> (map (`V.snoc` NoOp i) accDone ++ map (`V.snoc` Jump i) accToDo, map (`V.snoc` NoOp i) accToDo)
+              Jump i -> (map (`V.snoc` Jump i) accDone ++ map (`V.snoc` NoOp i) accToDo, map (`V.snoc` Jump i) accToDo)
+              Accum i -> (map (`V.snoc` Accum i) accDone, map (`V.snoc` Accum i) accToDo)
 
 
 --------------------------------------------------------------------------------
