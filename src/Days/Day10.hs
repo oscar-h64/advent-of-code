4030688d3f8dc3ecb7c6edfbe59b21021539c887
Oscar Harris <oscar@oscar-h.com>
2020-12-10 09:37:46 +0000

    Broken Day 10

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -4,6 +4,7 @@ module Days.Day10 (runDay) where
 
 --------------------------------------------------------------------------------
 
+import           Data.Bifunctor       ( first )
 import           Data.List
 import           Data.Map             ( Map )
 import qualified Data.Map             as M
@@ -17,6 +18,7 @@ import qualified Util.Util            as U
 
 import           Data.Attoparsec.Text
 
+import           Debug.Trace          ( traceShowId )
 import qualified Program.RunDay       as R ( runDay )
 
 --------------------------------------------------------------------------------
@@ -29,34 +31,61 @@ runDay = R.runDay inputParser partA partB
 --------------------------------------------------------------------------------
 
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = fmap ((0:) . sort) $ decimal `sepBy` endOfLine
 
 
 --------------------------------------------------------------------------------
 --                                   TYPES                                    --
 --------------------------------------------------------------------------------
 
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 
 --------------------------------------------------------------------------------
 --                                   PART A                                   --
 --------------------------------------------------------------------------------
 
+f :: (Int, Int, Int) -> Int -> [Int] -> (Int, Int, Int)
+f xs         _    []          = xs
+f (x1,x2,x3) last (next:rest) = case next - last of
+    1 -> f (x1+1, x2, x3) next rest
+    2 -> f (x1, x2+1, x3) next rest
+    3 -> f (x1, x2, x3+1) next rest
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = (\(x1,_,x3) -> (1+x3)*x1) . uncurry (f (0,0,0)) . first head . splitAt 1
 
 
 --------------------------------------------------------------------------------
 --                                   PART B                                   --
 --------------------------------------------------------------------------------
 
+-- CHAINS ARE CONSTRUCTED IN REVERSE - LARGEST AT FRONT
+
+findWith :: Int -> Int -> [Int] -> Maybe (Int, [Int])
+findWith _    _   []     = Nothing
+findWith last gap (x:xs) =
+    if | x - last == gap -> Just (x,xs)
+       | x - last > gap -> Nothing
+       | otherwise -> findWith last gap xs
+
+applyFindWith :: [Int] -> (Int, [Int]) -> ([Int], [Int])
+applyFindWith chain (x, xs) = (x:chain, xs)
+
+g :: ([Int], [Int]) -> [([Int], [Int])]
+g (chain, []) = pure (chain, [])
+g (chain, remaining) = let last = head chain
+                       in map (applyFindWith chain) $ catMaybes [findWith last 1 remaining, findWith last 2 remaining, findWith last 3 remaining]
+
+h :: [([Int], [Int])] -> [([Int], [Int])]
+h chains = if all (null . snd) chains then chains else h $ concatMap g chains
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = length $ filter ((== maximum input) . head) $ map fst $ h [([0], input)]
 
 
 --------------------------------------------------------------------------------
