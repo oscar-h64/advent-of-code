67d26c9e826d45c5802e5f16b3862128b7701b74
Oscar Harris <oscar@oscar-h.com>
2020-12-12 12:02:09 +0000

    Use state

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -4,6 +4,7 @@ module Days.Day12 (runDay) where
 
 --------------------------------------------------------------------------------
 
+import           Control.Monad.State
 import           Data.List
 import           Data.Map             ( Map )
 import qualified Data.Map             as M
@@ -59,28 +60,52 @@ type OutputB = Int
 
 
 --------------------------------------------------------------------------------
+--                                   SHARED                                   --
+--------------------------------------------------------------------------------
+
+class Day12State v where
+    nextDir :: State v ()
+
+    addOffset :: (Int, Int) -> State v ()
+
+    goForward :: Int -> State v ()
+
+    manhattanDist :: State v Int
+
+getManDist :: Day12State v => v -> [Instruction] -> Int
+getManDist init inp = evalState (mapM_ applyIns inp >> manhattanDist) init
+
+applyIns :: Day12State v => Instruction -> State v ()
+applyIns (Move North i) = addOffset ( i,  0)
+applyIns (Move East  i) = addOffset ( 0,  i)
+applyIns (Move South i) = addOffset (-i,  0)
+applyIns (Move West  i) = addOffset ( 0, -i)
+applyIns (Rotate i)     = replicateM_ (i `div` 90) nextDir
+applyIns (Forward i)    = goForward i
+
+
+--------------------------------------------------------------------------------
 --                                   PART A                                   --
 --------------------------------------------------------------------------------
 
-nextDirA :: Direction -> Direction
-nextDirA North = East
-nextDirA East = South
-nextDirA South = West
-nextDirA West = North
+instance Day12State (Int, Int, Direction) where
+    nextDir = modify $ \(x,y,d) -> case d of
+        North -> (x, y, East )
+        East  -> (x, y, South)
+        South -> (x, y, West )
+        West  -> (x, y, North)
 
-applyInsA :: Instruction -> (Int, Int, Direction) -> (Int, Int, Direction)
-applyInsA (Move North i) (x,y,d) = (x + i, y, d)
-applyInsA (Move East i) (x,y,d) = (x, y + i, d)
-applyInsA (Move South i) (x,y,d) = (x - i, y, d)
-applyInsA (Move West i) (x,y,d) = (x, y - i, d)
-applyInsA (Rotate i) (x,y,d) = (x, y, foldr (const nextDirA) d [1..i `div` 90])
-applyInsA (Forward i) (x,y,d) = applyInsA (Move d i) (x,y,d)
+    addOffset (xi,yi) = modify $ \(x, y, d) -> (x + xi, y + yi, d)
 
-manhattanDistA :: (Int, Int, a) -> Int
-manhattanDistA (x,y,_) = abs x + abs y
+    goForward i = get >>= \(_,_,d) -> applyIns (Move d i)
+
+    manhattanDist = get >>= \(x,y,_) -> pure $ abs x + abs y
+
+startStateA :: (Int, Int, Direction)
+startStateA = (0,0,East)
 
 partA :: Input -> OutputA
-partA = manhattanDistA . foldl' (flip applyInsA) (0,0,East)
+partA = getManDist startStateA
 
 
 --------------------------------------------------------------------------------
@@ -88,25 +113,20 @@ partA = manhattanDistA . foldl' (flip applyInsA) (0,0,East)
 --------------------------------------------------------------------------------
 
 -- First 2 offset from ship, second 2 position of waypoint
+instance Day12State (Int, Int, Int, Int, Direction) where
+    nextDir = modify $ \(x',y',x,y,d) -> (-y', x', x-x'-y', y-y'+x', d)
 
-nextDirB :: (Int, Int, Int, Int, Direction) -> (Int, Int, Int, Int, Direction)
-nextDirB (x',y',x,y,d) = (-y', x', x-x'-y', y-y'+x', d)
+    addOffset (xi,yi) = modify $ \(x',y',x,y,d) -> (x'+xi, y'+yi, x + xi, y+yi, d)
 
-applyInsB :: Instruction
-          -> (Int, Int, Int, Int, Direction)
-          -> (Int, Int, Int, Int, Direction)
-applyInsB (Move North i) (x',y',x,y,d) = (x'+i, y', x + i, y, d)
-applyInsB (Move East i) (x',y',x,y,d) = (x', y'+i, x, y + i, d)
-applyInsB (Move South i) (x',y',x,y,d) = (x'-i, y', x - i, y, d)
-applyInsB (Move West i) (x',y',x,y,d) = (x', y'-i, x, y - i, d)
-applyInsB (Rotate i) state = foldr (const nextDirB) state [1..i `div` 90]
-applyInsB (Forward i) (x',y',x,y,d) = (x', y', x+x'*i, y+y'*i, d)
+    goForward i = modify $ \(x',y',x,y,d) -> (x', y', x+x'*i, y+y'*i, d)
 
-manhattanDistB :: (Int, Int, Int, Int, a) -> Int
-manhattanDistB (x',y',x,y,_) = abs (x-x') + abs (y-y')
+    manhattanDist = get >>= \(x',y',x,y,_) -> pure $ abs (x-x') + abs (y-y')
+
+startStateB :: (Int, Int, Int, Int, Direction)
+startStateB = (1,10,1,10,East)
 
 partB :: Input -> OutputB
-partB = manhattanDistB . foldl' (flip applyInsB) (1,10,1,10,East)
+partB = getManDist startStateB
 
 
 --------------------------------------------------------------------------------
